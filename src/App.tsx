fdd2670b1aab0f783b0a54de9ab3a52225419035
srikanth <rmsrikanth25@gmail.com>
2025-08-29 12:55:58 +0530

    Testing Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="font-bold text-green-600">hello world</h1>
+      <h1 className="font-bold text-green-600">hello world Sriaknth</h1>
     </>
   );
 }
